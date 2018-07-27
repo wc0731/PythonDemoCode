bf833c7fcc9c40cb6b4a426e38f9669cea00db9e
James Chu <james.chu@cybertan.com.tw>
2018-07-27 14:52:27 +0800

    Fixed an issue in addition5.py

--- addition5.py
+++ addition5.py
@@ -1,7 +1,7 @@
-# It's a sample code created by James Chu.
+# It's a sample code which was created by James Chu.
 def sumProblem(x, y):
     sum = x + y
-    print('The sum of ', x, ' and ', y, ' is ', sum, '.', sep='')
+    print('The sum of ', x, ' and ', y, ' is ', sum, '.')
 
 def main():
     sumProblem(2, 3)
